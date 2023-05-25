27dfd9ba99e6ee972ff74bab4d78ad26d51ae334
Gustavo-s-machado <gustavo.smachado@sptech.school>
2023-05-24 21:02:23 -0300

    parametrização da api

--- site/public/js/funcoes.js
+++ site/public/js/funcoes.js
@@ -4,6 +4,7 @@ function validarSessao() {
 
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
+    var usu_img = sessionStorage.IMG_USUARIO;
 
     var b_usuario = document.getElementById("b_usuario");
 
